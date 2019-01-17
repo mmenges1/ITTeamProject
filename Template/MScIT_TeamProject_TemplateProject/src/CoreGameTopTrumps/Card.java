6470942370fdf353831ef0ea60f949f707f8a2df
Makayla Menges <mmenges3@gmail.com>
2019-01-17 06:48:35 +0000

    updated card.java

--- Template/MScIT_TeamProject_TemplateProject/src/CoreGameTopTrumps/Card.java
+++ Template/MScIT_TeamProject_TemplateProject/src/CoreGameTopTrumps/Card.java
@@ -1,50 +1,50 @@
 package CoreGameTopTrumps;
 
 public class Card {
-	protected String attribute1;
-	protected String attribute2;
-	protected String attribute3;
-	protected String attribute4;
-	protected String attribute5;
+	protected int attribute1;
+	protected int attribute2;
+	protected int attribute3;
+	protected int attribute4;
+	protected int attribute5;
 	protected String name;
-	
+
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getAttribute1() {
+	public int getAttribute1() {
 		return attribute1;
 	}
 	public void setAttribute1(String attribute1) {
 		this.attribute1 = attribute1;
 	}
-	public String getAttribute2() {
+	public int getAttribute2() {
 		return attribute2;
 	}
 	public void setAttribute2(String attribute2) {
 		this.attribute2 = attribute2;
 	}
-	public String getAttribute3() {
+	public int getAttribute3() {
 		return attribute3;
 	}
 	public void setAttribute3(String attribute3) {
 		this.attribute3 = attribute3;
 	}
-	public String getAttribute4() {
+	public int getAttribute4() {
 		return attribute4;
 	}
 	public void setAttribute4(String attribute4) {
 		this.attribute4 = attribute4;
 	}
-	public String getAttribute5() {
+	public int getAttribute5() {
 		return attribute5;
 	}
 	public void setAttribute5(String attribute5) {
 		this.attribute5 = attribute5;
 	}
-	
+
 	public void viewCard()
 	{
 		String cardString = "Name: " + getName() +
