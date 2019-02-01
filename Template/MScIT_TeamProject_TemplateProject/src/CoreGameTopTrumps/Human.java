d2f200956a6cdee843bc743d6ef385cc7bda502f
briancollins-92 <bcollins.sw@gmail.com>
2019-02-01 17:04:09 +0000

    changes made

--- Template/MScIT_TeamProject_TemplateProject/src/CoreGameTopTrumps/Human.java
+++ Template/MScIT_TeamProject_TemplateProject/src/CoreGameTopTrumps/Human.java
@@ -37,30 +37,30 @@ public class Human extends User {
 	
 	
 
-	public static void main(String[] args) {
-		Deck d = new Deck();
-		ArrayList<Card> list = new ArrayList<Card>();
-		ArrayList<Card> hand = new ArrayList<Card>();
-		Human kevin = new Human();
-		list = d.createDeck("StarCitizenDeck.txt");
-		//This method shuffles the list
-		Collections.shuffle(list);
-		for (int i = 0; i< 5; i++) {
-			Card c = list.get(i);
-			hand.add(c);
-		}
-		kevin.setHand(hand);
-		System.out.println(kevin.getHandSize());
-		//				kevin.displayEntireHand();
-		for (Card card : kevin.getHand()) {
-			System.out.println(card.viewCard());
-		}
-		while (kevin.getHandSize() != 0) {	
-			System.out.println(kevin.getHand().get(0).viewCard());
-			kevin.discardTopCard();
-			System.out.println(kevin);
-		}
-	}
+//	public static void main(String[] args) {
+//		Deck d = new Deck();
+//		ArrayList<Card> list = new ArrayList<Card>();
+//		ArrayList<Card> hand = new ArrayList<Card>();
+//		Human kevin = new Human();
+//		list = d.createDeck("StarCitizenDeck.txt");
+//		//This method shuffles the list
+//		Collections.shuffle(list);
+//		for (int i = 0; i< 5; i++) {
+//			Card c = list.get(i);
+//			hand.add(c);
+//		}
+//		kevin.setHand(hand);
+//		System.out.println(kevin.getHandSize());
+//		//				kevin.displayEntireHand();
+//		for (Card card : kevin.getHand()) {
+//			System.out.println(card.viewCard());
+//		}
+//		while (kevin.getHandSize() != 0) {	
+//			System.out.println(kevin.getHand().get(0).viewCard());
+//			kevin.discardTopCard();
+//			System.out.println(kevin);
+//		}
+//	}
 
 	@Override
 	String getCriteriaName(Card topCard) {
