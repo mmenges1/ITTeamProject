53ba074ed32ac1eb6d622aba85c49a824ada4c42
StephenScott1993 <46502101+StephenScott1993@users.noreply.github.com>
2019-02-16 14:16:31 +0000

    Update DBConnect.java

--- Template/MScIT_TeamProject_TemplateProject/src/CoreGameTopTrumps/DBConnect.java
+++ Template/MScIT_TeamProject_TemplateProject/src/CoreGameTopTrumps/DBConnect.java
@@ -3,15 +3,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-
-
+/**
+ * This class is responsible for establishing and closing a connection to a PostgreSQL database
+ */
 public class DBConnect {
 	private static Connection connection;
 	
 	public static void DBConnect() {
 		connectToTopTrumpsGameDataBase();
 	}
-	
+		
+	/*
+	 * This method establishes a connection to the Top Trumps database
+	 *  @returns the database connection
+	 */
 	public static Connection connectToTopTrumpsGameDataBase(){
 		String url = "jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/";
 		String userName = "m_18_2391985s";
@@ -30,6 +35,9 @@ public class DBConnect {
 	  	return connection;
 	}
 	
+	/*
+	 * This method closes the connection to the Top Trumps database
+	 */
 	public void closeConnectionToTopTrumpsGameDataBase() {
 		try {
 			connection.close();
